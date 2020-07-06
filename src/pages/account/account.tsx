a99ee39c72f8c8d5526e698fd82aec02f794d9ce
Raja Parsaulian B <raja.parsaulian@gmail.com>
2020-07-06 20:57:27 +0700

    ADD Login page

--- src/pages/account/account.tsx
+++ src/pages/account/account.tsx
@@ -5,7 +5,20 @@ import "./account.css";
 const Account: React.FC = () => {
   return (
     <React.Fragment>
-      Account
+      <div className="account-login">
+        <p>Login</p>
+        <form className="col-100">
+          <div>
+            <input type="text" placeholder="Username" />
+          </div>
+          <div>
+            <input type="password" placeholder="Password" />
+          </div>
+          <div>
+            <input type="submit" value="Login" />
+          </div>
+        </form>
+      </div>
       <DefaultNavbar />
     </React.Fragment>
   );
